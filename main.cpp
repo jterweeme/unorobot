18afea23ab6426c13a02153aaa37c0d02730c46c
Jasper ter Weeme <jasper@nbweeme.lan>
2013-05-09 15:39:15 +0200

    documentatie stub

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
+#include <stdlib.h>
+#include <avr/io.h>
+void * operator new (size_t size)
+{
+    return malloc(size);
+}
+
+class Servo
+{
+public:
+    Servo();
+};
+
 class PanTilt
 {
 public:
     PanTilt();
+private:
+    Servo *pan;
+    Servo *tilt;
 };
 
 class Robot
@@ -12,8 +28,21 @@ private:
     PanTilt *pt;
 };
 
+Servo::Servo()
+{
+    TCCR2B = (1<<WGM01) | (1<<COM2B0) | (1<<CS01);
+    OCR2B = 24;
+    DDRD = (1<<3);
+}
+
+PanTilt::PanTilt()
+{
+    pan = new Servo();
+}
+
 Robot::Robot()
 {
+    pt = new PanTilt();
 }
 
 int main()
